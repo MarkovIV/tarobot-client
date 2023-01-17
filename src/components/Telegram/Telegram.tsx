7a1a8367fc3ebaa591e839f8c711132c241bc8ea
Ivan <mrkvvn@yandex.ru>
2023-01-17 13:24:24 +0300

    Telegram addons

--- src/components/Telegram/Telegram.tsx
+++ src/components/Telegram/Telegram.tsx
@@ -33,44 +33,48 @@ export const Telegram = ({ className, ...props }: TelegramProps): JSX.Element =>
 	}
 
 	return (
-		<div className="">
+		<div>
 			<div className="absolute left-0 top-0 flex justify-start items-start w-full h-full min-w-[350px] min-h-[350px]">	
 				<img className="object-cover w-full h-full" src={"../../pics/backgroundstars.jpg"} alt="Background" />
 			</div>
-			<Typography
-				id="modal-modal-title"
-				variant="h6"
-				component="h2"
-				color="#e0e0e0"
-				sx={{
-					textAlign: 'center',
-					paddingTop: '30px',
-					zIndex: '10'
-				}}
-			>
-				Карта дня
-			</Typography>
-			<div className="flex w-full justify-center items-center">
-				<Typography
-					variant="subtitle1"
-					gutterBottom
-					color="#e0e0e0"
-					sx={{ zIndex: '10' }}
-				>
-					{getClientDataByTgName(user.username).firstName}
-				</Typography>	
-			</div>
-			<div className="flex w-full justify-center items-center">
-				<Typography
-					variant="subtitle1"
-					gutterBottom
-					color="#e0e0e0"
-					sx={{ zIndex: '10 '}}
-				>
-					{dayjs().locale('ru').format('dd DD MMMM YYYY г.')}
-				</Typography>	
-			</div>
-			<DaycardEdit onClose={onClose} clientId={getClientDataByTgName(user.username).id} date={dayjs()} view />
+			{	getClientDataByTgName(user.username).id &&
+				<div>
+					<Typography
+						id="modal-modal-title"
+						variant="h6"
+						component="h2"
+						color="#e0e0e0"
+						sx={{
+							textAlign: 'center',
+							paddingTop: '30px',
+							zIndex: '10'
+						}}
+					>
+						Карта дня
+					</Typography>
+					<div className="flex w-full justify-center items-center">
+						<Typography
+							variant="subtitle1"
+							gutterBottom
+							color="#e0e0e0"
+							sx={{ zIndex: '10' }}
+						>
+							{getClientDataByTgName(user.username).firstName}
+						</Typography>	
+					</div>
+					<div className="flex w-full justify-center items-center">
+						<Typography
+							variant="subtitle1"
+							gutterBottom
+							color="#e0e0e0"
+							sx={{ zIndex: '10 '}}
+						>
+							{dayjs().locale('ru').format('dd DD MMMM YYYY г.')}
+						</Typography>	
+					</div>
+					<DaycardEdit onClose={onClose} clientId={getClientDataByTgName(user.username).id} date={dayjs()} view />
+				</div>
+			}
 		</div>
 	)
 }

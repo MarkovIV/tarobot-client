2b1355e3e884ca390f30e75d471c8e42c000a65c
Ivan <mrkvvn@yandex.ru>
2023-01-17 12:42:18 +0300

    Telegram addons

--- src/components/Telegram/Telegram.tsx
+++ src/components/Telegram/Telegram.tsx
@@ -1,55 +1,76 @@
 import { TelegramProps } from './Telegram.props'
 import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import LoginIcon from '@mui/icons-material/Login'
 import { useTelegram } from '../../hooks/useTelegram'
 import { useEffect } from 'react'
+import dayjs from 'dayjs'
+import { useAppSelector } from '../../store/hooks'
+import { DaycardEdit } from '../DaycardEdit/DaycardEdit'
 
 export const Telegram = ({ className, ...props }: TelegramProps): JSX.Element => {
-	const { onClose, tg, queryId, user } = useTelegram()
+	const { onClose, tg, user } = useTelegram()
+	const clients = useAppSelector(state => state.clients.clients)
 
-	
 	useEffect(() => {
-		//debug
-		console.log('tg', tg)
-		console.log('queryId', queryId)
-		console.log('user', user)
-
 		tg.MainButton.hide()
 		tg.MainButton.disable()
 		tg.MainButton.hideProgress()
 		tg.expand()
+	}, [tg])
 
-		return () => {
-			
+	const getClientDataByTgName = (tgName: string) => {
+		let clientData = {
+			id: '',
+			firstName: ''
 		}
-	}, [])
+
+		const tgClient = clients.find(cl => cl.telegram === tgName)
+		if (tgClient) {
+			clientData.id = tgClient.id
+			clientData.firstName = tgClient.firstName
+		}
+
+		return clientData
+	}
 
 	return (
-		<div className="absolute left-0 top-0 flex flex-col items-center justify-center flex-nowrap w-full h-full min-w-[350px] min-h-[350px]">
-			<div className="h-1/6 w-full"></div>
-			<div className="h-4/6 w-full md:w-3/4 lg:w-4/12 flex items-center justify-start flex-col pl-3 pr-3">
-				<div className="flex">
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="telegram"
-						onClick={onClose}
-					>
-						<LoginIcon sx={{ fill: '#e0e0e0', }}/>
-						<Typography
-							variant="h4"
-							color="#e0e0e0"
-							gutterBottom
-							sx={{ margin: '0', paddingLeft: '10px', }}
-						>
-							Hello, Telegram!
-						</Typography>
-					</IconButton>
-				</div>
+		<div className="">
+			<div className="absolute left-0 top-0 flex justify-start items-start w-full h-full min-w-[350px] min-h-[350px]">	
+				<img className="object-cover w-full h-full" src={"../../pics/backgroundstars.jpg"} alt="Background" />
 			</div>
-			<div className="h-1/6 w-full"></div>
+			<Typography
+				id="modal-modal-title"
+				variant="h6"
+				component="h2"
+				color="#e0e0e0"
+				sx={{
+					textAlign: 'center',
+					paddingTop: '30px',
+					zIndex: '10'
+				}}
+			>
+				Карта дня
+			</Typography>
+			<div className="flex w-full justify-center items-center">
+				<Typography
+					variant="subtitle1"
+					gutterBottom
+					color="#e0e0e0"
+					sx={{ zIndex: '10' }}
+				>
+					{getClientDataByTgName(user.username).firstName}
+				</Typography>	
+			</div>
+			<div className="flex w-full justify-center items-center">
+				<Typography
+					variant="subtitle1"
+					gutterBottom
+					color="#e0e0e0"
+					sx={{ zIndex: '10 '}}
+				>
+					{dayjs().locale('ru').format('dd DD MMMM YYYY г.')}
+				</Typography>	
+			</div>
+			<DaycardEdit onClose={onClose} clientId={getClientDataByTgName(user.username).id} date={dayjs()} view />
 		</div>
 	)
 }

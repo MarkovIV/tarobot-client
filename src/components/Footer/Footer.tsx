92a8a89999df5de2f0074716ee105c2dac7540ef
Ivan <mrkvvn@yandex.ru>
2023-01-19 15:11:12 +0300

    Telegram addons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,24 @@
 import { FooterProps } from './Footer.props'
 import Typography from '@mui/material/Typography'
 import CopyrightIcon from '@mui/icons-material/Copyright'
+import { useTelegram } from '../../hooks/useTelegram'
 
 export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	const {tg} = useTelegram()
 
 	return (
-		<div className="fixed z-20 bottom-5 left-3 flex flex-nowrap h-10 min-w-max bg-inherit w-full text-gray-300">
-			<div className="mr-1">
-				<CopyrightIcon />
-			</div>
-			<Typography variant="subtitle1" gutterBottom>
-			Маркова Евгения
-			</Typography>		
+		<div>
+			{	(tg.platform === 'unknown') &&
+
+				<div className="fixed z-20 bottom-5 left-3 flex flex-nowrap h-10 min-w-max bg-inherit w-full text-gray-300">
+					<div className="mr-1">
+						<CopyrightIcon />
+					</div>
+					<Typography variant="subtitle1" gutterBottom>
+					Маркова Евгения
+					</Typography>		
+				</div>
+			}
 		</div>
 	)			
 }
